35c5c54db6967986ca0638a0712f9b5a26f93c4f
Gabe McBride <glmcbr06@gmail.com>
2024-01-18 10:01:35 -0800

    testing release version

---
+++ semantic-release.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    branches: ['master', {name: 'feat/*', prerelease: 'dev'}],
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/github',
+      [
+        '@semantic-release/changelog',
+        {
+          changelogFile: 'CHANGELOG.md',
+        },
+      ],
+      [
+        '@semantic-release/git',
+        {
+          assets: ['CHANGELOG.md'],
+          message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        },
+      ],
+    ],
+  };
+  
